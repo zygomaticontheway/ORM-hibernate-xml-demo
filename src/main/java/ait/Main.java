bd49b1305f63d6b955dcdf20024f35688adef7ff
zygomaticontheway <mrzygomatic@gmail.com>
2024-08-22 13:03:30 +0200

    hibernate lesson 2

--- src/main/java/ait/Main.java
+++ src/main/java/ait/Main.java
@@ -1,10 +1,19 @@
 package ait;
 
+import ait.entity.User;
+
 public class Main {
     public static void main(String[] args) {
         HibernateConfiguration hibernateConfiguration = new HibernateConfiguration();
         UserXmlHibernateRepository repository = new UserXmlHibernateRepository(hibernateConfiguration);
 
         System.out.println(repository.findAll());
+        System.out.println(repository.findById(2));
+
+        System.out.println("---------save--------");
+        User newUser = repository.create(new User(null, "Masha", "masha@ma.ma", "pwdnmksdf"));
+        System.out.println(newUser);
+        System.out.println(repository.findAll());
+
     }
 }
--- src/main/java/ait/UserXmlHibernateRepository.java
+++ src/main/java/ait/UserXmlHibernateRepository.java
@@ -2,6 +2,7 @@ package ait;
 
 import ait.entity.User;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 
 import java.util.List;
 
@@ -14,8 +15,33 @@ public class UserXmlHibernateRepository {
         entityManager = hibernateConfiguration.getEntityManager();
     }
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         //достаем запрос в таблицу Users из entityManager
         return entityManager.createQuery("from User", User.class).getResultList();
     }
+
+    public User findById(Integer id) {
+        return entityManager.find(User.class, id);
+    }
+
+    public User create(User user) {
+
+        //создаем объект hibernate, который будет хранить операции и если весь процесс завершится неудачей, то транзакция откатится к моменту до ее создания
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try {
+            transaction.begin();
+            entityManager.persist(user); //метод сохранения
+            transaction.commit(); //завершить транзакцию
+            return user;
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            throw new RuntimeException("Create new user fail!: " + user);
+        }
+
+    }
+
 }
